13e68a9cfb6134adc0357425749144ae6606d7eb
VenkateshGopa <85928554+VenkateshGopa@users.noreply.github.com>
2021-11-17 22:06:44 +0530

    Update Verification.js

--- src/components/Auth/forgot password/Verification.js
+++ src/components/Auth/forgot password/Verification.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from 'axios';
 import classes from '../Rlf.module.css';
 import { useHistory ,useParams } from "react-router";
@@ -7,8 +7,33 @@ const Verification = () =>{
     const [verificationdata, setdata] = useState({});
     const [status , setstatus] =useState(null);
     const [err , seterr] =useState(null);
+    const [loding ,  setloding] = useState(true);
+    const [valid ,setvalid] =useState(true);
+
     const params = useParams();
     const his = useHistory();
+
+    useEffect(()=>{
+
+        const date = window.Date.now()
+        setloding(true)
+        const fetch = async() => {
+        try{
+            await axios.post(`https://auth7799.herokuapp.com/linkvalid`, {id:params.id , time: date});
+            setvalid(true)
+            setloding(false)
+        }
+        catch(error){
+            setvalid(false)
+            setloding(false)
+        }
+    }
+    fetch()
+    },[params.id])
+
+    const redirect = () =>{
+        his.push('/forgotpassword');
+    }
     const OtpchangeHandler= ({ target: { name, value } }) =>{
         setdata( prev =>({...prev ,  [name]:value}))
         setstatus(null);
@@ -46,6 +71,8 @@ const Verification = () =>{
     }
     return(
         <Layout className={classes.form}>
+        {loding && <p className={classes.label}>...loading</p>}
+        {!loding && ( !valid ? (<><p className={classes.label}>link expired</p> <button className='btn btn-primary form-control mb-4' onClick={redirect}>Resend Link</button> </>) :(<>
         <p className={classes.heading}> Change Password </p>
         <form onSubmit={submithandler}>
             <p className={classes.label}> OTP - One Time Password </p>
@@ -57,7 +84,7 @@ const Verification = () =>{
             <input type='password' className='form-control mb-3' onChange={changeHandler} name='password1' placeholder='New Conform password'  autoComplete="new-password"/>
             {err && <p className={classes.errormessage}>{err}</p>}
             <button className='btn btn-primary form-control mb-4' >Change Password</button>
-        </form>
+        </form></>))}
         </Layout>
     );
 }
